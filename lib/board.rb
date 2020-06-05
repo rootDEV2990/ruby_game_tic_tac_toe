1d4cdc42d4f2d86928579637204d4916255e67d7
rootDEV2990 <website.dev@icloud.com>
2020-06-05 10:25:52 -0500

    Added file structure for 1st mile stone

---
+++ lib/board.rb
@@ -0,0 +1,15 @@
+class Board 
+    def initialize
+        @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    end
+
+    def display
+        puts "| #{@board[0]} | #{@board[1]} | #{@board[2]} |"
+        puts " ---+---+--- "
+        puts "| #{@board[3]} | #{@board[4]} | #{@board[5]} |"
+        puts " ---+---+--- "
+        puts "| #{@board[6]} | #{@board[7]} | #{@board[8]} |"
+    end
+end
+
+puts Board.new.display
