a2ee2d44da8b4acf0b9036a1054c99ac260c1872
rootDEV2990 <website.dev@icloud.com>
2020-06-09 08:48:01 -0500

    Added Game Class with player start board printing and testing on turn swapping

--- lib/board.rb
+++ lib/board.rb
@@ -4,11 +4,13 @@ class Board
   end
 
   def display
+    puts ' ---+---+--- '
     puts "| #{@board[0]} | #{@board[1]} | #{@board[2]} |"
     puts ' ---+---+--- '
     puts "| #{@board[3]} | #{@board[4]} | #{@board[5]} |"
     puts ' ---+---+--- '
     puts "| #{@board[6]} | #{@board[7]} | #{@board[8]} |"
+    puts ' ---+---+--- '
   end
 end
 
