6f6f21a784e59b05a898f3358772203062bbd30a
a2josue@yahoo.com <a2josue@yahoo.com>
2020-06-28 04:44:03 -0400

    modified board.rb file

---
+++ lib/board.rb
@@ -0,0 +1,24 @@
+
+    class Board
+        attr_accessor :board
+    
+        def initialize
+          @board = Array.new(9, " ")
+        end
+    
+        def game_board 
+          puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+          puts "-----------"
+          puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+          puts "-----------"
+          puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+        end
+    
+        def change_index(position, index)
+          @board[position] = index
+        end
+    
+        def position_taken?(position)
+          return @board[position] == "X" || @board[position] == "O"
+        end
+      end  
