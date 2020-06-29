5ce732a1987c6713a7ee5d1679d3ca8c9c859ea9
rootDEV2990 <website.dev@icloud.com>
2020-06-29 11:32:29 -0500

    Del empty line

--- lib/game.rb
+++ lib/game.rb
@@ -4,7 +4,6 @@ require_relative '../lib/player.rb'
 class Game
   attr_accessor :player1, :player2, :turn, :board, :winning_combinations
 
-
   def initialize(player1, player2, board)
     @player1 = player1
     @player2 = player2
