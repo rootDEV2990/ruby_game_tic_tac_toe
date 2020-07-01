306e0c7eaafe8ab776aa42d0b155ec9d2f8494ee
rootDEV2990 <website.dev@icloud.com>
2020-07-01 16:32:34 -0500

    Fixing rubocop

--- lib/board.rb
+++ lib/board.rb
@@ -7,9 +7,9 @@ class Board
 
   def game_board
     play_board = [
-    " #{@board[0]} | #{@board[1]} | #{@board[2]} ",
-    "-----------",
-    " #{@board[3]} | #{@board[4]} | #{@board[5]} ",
+      " #{@board[0]} | #{@board[1]} | #{@board[2]} ",
+      "-----------",
+      " #{@board[3]} | #{@board[4]} | #{@board[5]} ",
     "-----------",
     " #{@board[6]} | #{@board[7]} | #{@board[8]} "
     ]
