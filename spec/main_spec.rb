2c28c09b9f4567867fa4f65d3a6780be3fe8f58a
chindrop <apronticaleb@gmail.com>
2020-07-29 17:03:57 +0000

    Test start

---
+++ spec/main_spec.rb
@@ -0,0 +1,15 @@
+require './lib/board.rb'
+require './lib/player.rb'
+
+describe Board do
+
+  describe '.game_board' do
+    it 'Replacing index on the game board' do
+      p @board[0]
+      #expect(subject).to  
+    end
+    
+  end
+  
+  
+end
